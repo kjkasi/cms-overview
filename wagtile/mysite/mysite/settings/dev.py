b7b28ac136c4837f72cfc173e29d5f03b13d79cb
kjkasi <5842602+kjkasi@users.noreply.github.com>
2025-10-06 22:48:52 +0300

    add projects

---
+++ wagtile/mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-h#mp$b+#)!t!ytag__5m&1&!5y9*qck3d70ab!i8=p#r(s3(az"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
